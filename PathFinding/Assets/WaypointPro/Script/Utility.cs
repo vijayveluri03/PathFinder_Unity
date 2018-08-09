dc0703e576a92118af7a1ebd369d3e08c4f25895
Vijay Veluri <vijayveluri03@bitbucket.com>
2018-08-09 14:17:16 +0530

    forgot to commit utility file before

---
+++ PathFinding/Assets/WaypointPro/Script/Utility.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+	public static GUIStyle GetStyleWithRichText ( GUIStyle style = null )
+	{
+		style = style != null ? style : new GUIStyle();
+		style.richText = true;
+		return style;
+	}
+
+}
